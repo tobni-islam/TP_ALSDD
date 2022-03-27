6796d913da0ab7bf22dc56f8f2ad795e396b2444
tobni-islam <islam.tobni@gmail.com>
2022-03-27 20:04:22 +0100

    suuuuuu

--- TP.c
+++ TP.c
@@ -1,3 +1,16 @@
+/* hayla kader khedma mliha ajbetni , hayla yaser lokan nakhadmo haka rah navanciu mlih.
+1. manhtajouch les fonction:  acces_pos, acces_valeur, inserer, supp_pos
+
+2. la fonction 'inserer'  normalment manhtajohach pcq ida konna rah nzdo athlethe f une liste 
+ahsen tariqa hia nzidoh mn loul , mathemech lposition pcq aslan liste d'Athlethes mch ordonnes.
+donc makallah nparcouri tous la list pour inserer une athlethe , ndirouh fi blaste la tete directe.
+
+3. rani badelt le nom ta3 le pointeur t3 la tete fl procedure 'create' mn 'p' l 'tete' + badelt chwi f lfonction
+mba3d nzid nfahmek fiha
+
+4. mzal kayn problem fi random mba3d ntfahmo 3lih
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include <string.h>
@@ -16,6 +29,9 @@ typedef athlethe  * ptr ; // nouveau type pour les pointeures ptr khirmn point
 #define null NULL  // hadi b3thali aymen mb3d nfhmk fiha 
 const unsigned MAX = 10;
 const int nbCountries = 195;
+
+const int maxpays = 150;
+const int maxepreuve = 200;
 
 // reads n lines of a text file
 //returns 0 if an error occurs
@@ -67,29 +83,28 @@ void aff_nom (ptr p , char n[50] )
     strcpy(p->nom,n) ;
 }
 
-
+//modifite fiha chwy
 ptr create()
 {
-    ptr p,tmp,nouv ;
+    FILE* fichier;
+    fichier = fopen("noms","r");// fichier 'noms' nhoto fih les noms li nehtajouhm
+    ptr tete,tmp,nouv ;
     int n ;
     string x,y ;
-    srand(time(0));
+    srand(time(NULL));
     n = rand() % 11 ;
-    if (n==0)
-    {
-        return  0 ;
-    }
-    p = NULL ;
+    tete = NULL ;
     for (int i=0 ; i< n ; i++ )
     {
-        nouv = malloc(sizeof (athlethe));
-        nouv->suiv= NULL ;
-        scanf("%s", y);
+        nouv = malloc(sizeof(athlethe));
+        nouv->suiv =NULL ;
+        fgets(y,25,fichier);
         strcpy(nouv->nom, y);
-        if ( p==NULL)
+        if ( tete==NULL)
         {
-            p = nouv ;
+            tete = nouv ;
             tmp = nouv ;
+            nouv = NULL;
         }
         else {
             tmp->suiv = nouv;
@@ -97,34 +112,10 @@ ptr create()
             nouv = NULL;
         }
     }
-    return p ;
+    fclose(fichier);
+    return tete ;
 }
 
-ptr acces_pos (ptr p , int pos)
-{
-    ptr tmp;
-    tmp = p ;
-    int  cpt = 0 ;
-    while ( tmp != NULL) {
-        cpt += 1 ;
-        if ( cpt == pos ) return tmp ;
-        tmp = tmp->suiv ;
-    }
-}
-
-
-ptr acces_val(ptr p , string val)
-{
-    ptr tmp ;
-    tmp = p ;
-    while ( tmp != NULL)
-    {
-        if (tmp->nom == val ) return tmp ;
-        tmp = tmp ->suiv ;
-    }
-    printf ("la valeur n'existe pas ");
-    return NULL ;
-}
 
 void afficher(ptr p)
 {
@@ -137,26 +128,6 @@ void afficher(ptr p)
         tmp = tmp -> suiv ;
     }
     printf("\n") ;
-}
-
-void inserer(ptr p , int pos , string n  )
-{
-    ptr tmp,next ,prec;
-    tmp = malloc(sizeof (athlethe));
-    strcpy(tmp->nom , n );
-    prec = acces_pos(p,pos-1) ;
-    next = prec-> suiv ;
-    tmp -> suiv = next ;
-    prec->suiv = tmp ;
-}
-
-void supp_pos (ptr tete , int n )
-{
-    ptr prec ,p ;
-    prec = acces_pos(tete , n-1 );
-    p = prec->suiv  ;
-    prec->suiv = p-> suiv;
-    free(p);
 }
 
 void supp_nom (ptr tete , string n )
@@ -193,6 +164,43 @@ int longeur(ptr p)
     return cpt ;
 }
 
+
+//construire tableau tabpays
+void creer_tabpays(char tabpays[maxpays][255]) //theb mba3d nfehmek fiha mlih
+{
+    char pay[255];
+    FILE* fichier;
+    fichier = fopen("pays","r");
+    for(int i =1;i<maxpays;i++){
+        fgets(pay,255,fichier);
+        strcpy(tabpays[i],pay);
+    }
+    fclose(fichier);
+}
+
+//construire tableau tabepreuve
+void creer_tabepreuve(char tabepreuve[maxepreuve][255]) 
+{
+    char epreuve[255];
+    FILE* fichier;
+    fichier = fopen("epreuve","r");
+    for(int i =0;i<maxepreuve;i++){
+        fgets(epreuve,255,fichier);
+        strcpy(tabepreuve[i],epreuve);
+    }
+    fclose(fichier);
+}
+//construite le procedure creer tabjo
+
+void creer_tabjo(ptr tabjo[maxepreuve][maxpays])
+{
+    for(int i=0;i< maxepreuve;i++){
+        for(int j=0;j<maxpays;j++){
+            tabjo[i][j] = create();
+        }
+    }
+}
+
 int main()
 {
     int n ;
@@ -201,5 +209,25 @@ int main()
     p = create() ;
     afficher(p) ;
     printf("dsds %d ",longeur(p) ) ;
+    //test tabepreuve et tabpays
+    printf("\nles pays: \n");
+    char tabpays[150][255];
+    char tabpreuve[200][255];
+    creer_tabpays(tabpays);
+    creer_tabepreuve(tabpreuve);
+    for(int i =0;i< 150;i++){
+        printf("%s",tabpays[i]);
+    }
+    printf("\nles epreuves : ");
+    for(int i =0;i<200;i++){
+        printf("%s",tabpreuve[i]);
+    }
+    //test tabjo
+    ptr tabjo[maxepreuve][maxpays];
+    creer_tabjo(tabjo);
+    printf("\n");
+    afficher(tabjo[0][0]);
+    afficher(tabjo[21][32]);
+    afficher(tabjo[23][130]);
     return 0;
 }
